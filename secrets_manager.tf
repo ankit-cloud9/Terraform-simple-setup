0efbabcaf8f3db1059270d497074c86867631085
ankit-cloud9 <ankitchandanpat.niveda@gmail.com>
2026-05-01 11:24:55 +0530

    Update secrets_manager.tf

--- secrets_manager.tf
+++ secrets_manager.tf
@@ -1,10 +1,10 @@
-resource "aws_secretsmanager_secret" "main" {
+resource "aws_secretsmanager_secret" "new" {
   name = "project/dev/secret-v2"
   recovery_window_in_days = 0
 }
 
-resource "aws_secretsmanager_secret_version" "main_value" {
-  secret_id = aws_secretsmanager_secret.main.id
+resource "aws_secretsmanager_secret_version" "new_value" {
+  secret_id = aws_secretsmanager_secret.new.id
 
   secret_string = jsonencode({
     rds_endpoint = aws_db_instance.rds.endpoint
